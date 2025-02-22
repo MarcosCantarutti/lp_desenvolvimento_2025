066c9854bb1937e801d371251a275e76ca8b7f4e
Marcos Cantarutti <marcossandim21@gmail.com>
2025-02-22 19:14:00 -0300

    inicio

--- src/components/BlogSection/index.vue
+++ src/components/BlogSection/index.vue
@@ -1,26 +1,17 @@
 <template>
   <section class="w-full bg-gray-100 p-4">
-    <Carousel :items-to-show="3" :wrap-around="true">
-      <Slide v-for="(blog, index) in blogs" :key="index">
-        <BlogCard
-          :image="blog.image"
-          :title="blog.title"
-          :description="blog.description"
-          :link="blog.link"
-          :icon="blog.icon"
-        />
-      </Slide>
-      <template #addons>
-        <Navigation />
-        <Pagination />
-      </template>
-    </Carousel>
+    <BlogCard
+      v-for="blog in blogs"
+      :image="blog.image"
+      :title="blog.title"
+      :description="blog.description"
+      :link="blog.link"
+      :icon="blog.icon"
+    />
   </section>
 </template>
 
 <script setup lang="ts">
-import { Carousel, Slide, Navigation, Pagination } from 'vue3-carousel';
-import 'vue3-carousel/dist/carousel.css';
 import BlogCard from '@/components/BlogCard/index.vue';
 
 const blogs = [
